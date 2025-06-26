28330f76bc970d8866118b4505ee5a3662f0188f
fetalalcoholsyndrome23 <doh@parrastu.catholic.edu.au>
2025-06-26 11:18:56 +1000

    created GET and FORM requests to input valid data into the database

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,10 @@
 #standard routes for the website, where the user can go to
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Flask, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", boolean=True)
 
@@ -11,6 +12,24 @@ def login():
 def logout():
     return render_template("logout.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstname')
+        password = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 2 characters.', category='error')
+        elif password != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(password) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            # Add user to database
+            flash('Account created!', category='success')
+            
     return render_template("sign_up.html")
