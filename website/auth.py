40e54853a2bb31cd73a64d4e7b454ead315254b9
fetalalcoholsyndrome23 <doh@parrastu.catholic.edu.au>
2025-07-17 15:53:42 +1000

    Completed base website template, will continue to add more features

--- website/auth.py
+++ website/auth.py
@@ -1,35 +1,66 @@
-#standard routes for the website, where the user can go to
-from flask import Blueprint, render_template, Flask, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db   ##means from __init__.py import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", boolean=True)
-
-@auth.route('/logout')
-def logout():
-    return render_template("logout.html")
-
-@auth.route('/sign-up', methods=['GET', 'POST'])
-def signup():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstname')
-        password = request.form.get('password1')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
-            flash('Email must be greater than 4 characters.', category='error')
-        elif len(firstName) < 2:
-            flash('First name must be greater than 2 characters.', category='error')
-        elif password != password2:
-            flash('Passwords do not match.', category='error')
-        elif len(password) < 7:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            # Add user to database
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            
-    return render_template("sign_up.html")
+            return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html", user=current_user)
---
+++ website/static/darkmode.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let darkmode = localStorage.getItem('darkmode');
+  const themeSwitch = document.getElementById('theme-switch');
+
+  const enableDarkmode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+  };
+
+  const disableDarkmode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', 'null');
+  };
+
+  if (darkmode === "active") enableDarkmode();
+
+  if (themeSwitch) {
+    themeSwitch.addEventListener('click', () => {
+      darkmode = localStorage.getItem('darkmode');
+      darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+    });
+  }
+});
